123928e880281b8f0e22c8d8aaeec56549dec912
Giulia <giulia.brugnatti@studio.unibo.it@users.noreply.github.com>
2022-05-06 23:48:37 +0200

    test updated: forgot to test inherited method

--- src/test/kotlin/FluentBasedGoalTest.kt
+++ src/test/kotlin/FluentBasedGoalTest.kt
@@ -3,23 +3,28 @@ import io.mockk.every
 import io.mockk.mockk
 import io.mockk.mockkClass
 import org.junit.Test
+import resources.res.state
 
 class FluentBasedGoalTest{
 
     private val fluentBasedGoalEmpty: FluentBasedGoal= mockkClass(FluentBasedGoal::class){
         every { fluent } returns emptySet()
+        every { isStatisfiedBy(state) } returns false
     }
 
     private val fluentBasedGoalNotEmpty: FluentBasedGoal= mockkClass(FluentBasedGoal::class){
         every { fluent } returns mockk(relaxed=true)
+        every { isStatisfiedBy(state) } returns true
     }
     @Test
     fun testEmptyCreation(){
         fluentBasedGoalEmpty.fluent.isEmpty() shouldBe true
+        fluentBasedGoalEmpty.isStatisfiedBy(state) shouldBe false
     }
     @Test
     fun testNotEmptyCreation(){
         fluentBasedGoalNotEmpty.fluent.isEmpty() shouldBe false
+        fluentBasedGoalEmpty.isStatisfiedBy(state) shouldBe true
     }
 
 }

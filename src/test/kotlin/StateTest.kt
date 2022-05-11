6366e98879629e68d70d5065fb4a3efb9402a8d8
Giulia <giulia.brugnatti@studio.unibo.it@users.noreply.github.com>
2022-05-11 11:25:25 +0200

    remove useless variable

--- src/test/kotlin/StateTest.kt
+++ src/test/kotlin/StateTest.kt
@@ -3,30 +3,31 @@ import io.mockk.every
 import io.mockk.mockk
 import io.mockk.mockkClass
 import org.junit.Test
-import resources.res.action
+import resources.res.actionEmpty
+import resources.res.actionNotEmpty
 
 class StateTest {
     private val stateEmpty: State = mockkClass(State::class){
         every { fluents } returns emptySet()
-        every { isApplicable(action)} returns false
+        every { isApplicable(actionEmpty)} returns false
         //every { apply(state)} returns action
     }
     private val stateNotEmpty: State = mockkClass(State::class){
         every { fluents } returns mockk(relaxed =true)
-        every { isApplicable(action)} returns true
+        every { isApplicable(actionNotEmpty)} returns true
         //every { apply(state)} returns action
     }
     @Test
     fun testEmptyCreation() {
         stateEmpty.fluents.isEmpty() shouldBe true
-        stateEmpty.isApplicable(action) shouldBe false
+        stateEmpty.isApplicable(actionEmpty) shouldBe false
         //stateEmpty.apply(action) shouldBe state
     }
 
     @Test
     fun testNotEmptyCreation() {
         stateNotEmpty.fluents.isEmpty() shouldBe false
-        stateNotEmpty.isApplicable(action) shouldBe true
+        stateNotEmpty.isApplicable(actionNotEmpty) shouldBe true
         //assertEquals(stateEmpty.apply(action), state)
     }
 }

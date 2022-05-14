392dc713bdc4a1aa2b7be9f930ca819704b0c402
Giulia <giulia.brugnatti@studio.unibo.it@users.noreply.github.com>
2022-05-14 10:20:29 +0200

    :bug: fix ObjectTest

--- src/main/kotlin/Object.kt
+++ src/main/kotlin/Object.kt
@@ -1,12 +1,10 @@
 import impl.ObjectImpl
-
+import it.unibo.tuprolog.core.Atom
 //TODO scrivi qualcosa di sensato
 interface Object : Value {
     val representation: String
     override fun apply(substitution: Substitution): Object
-    /*companion object {
-        fun of(representation: String): Object = ObjectImpl()
+    companion object {
+        fun of(delegate: Atom): Object = ObjectImpl(delegate)
     }
-
-     */
 }

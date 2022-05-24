f3977af2f93cf7ac9026b7ae1cb421b27fd4a28d
Giovanni Ciatto <giovanni.ciatto@unibo.it>
2022-05-24 16:49:52 +0200

    chore!: rename FluentBasedGoal.{fluent -> fluents}

--- src/main/kotlin/FluentBasedGoal.kt
+++ src/main/kotlin/FluentBasedGoal.kt
@@ -1,11 +1,10 @@
 import impl.FluentBasedGoalImpl
 
-//TODO: partorire un nome meno orrido; riguarda/richiedi il significato.
 /**
- *
+ * TODO: partorire un nome meno orrido; riguarda/richiedi il significato.
  */
 interface FluentBasedGoal : Goal, Applicable<FluentBasedGoal> {
-    val fluent: Set<Fluent>
+    val fluents: Set<Fluent>
     companion object {
         fun of(fluent: Set<Fluent>): FluentBasedGoal = FluentBasedGoalImpl(fluent)
     }

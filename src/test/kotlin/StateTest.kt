f04724691e6f3b85b604512665ba22423155a44f
Giulia <giulia.brugnatti@studio.unibo.it@users.noreply.github.com>
2022-05-25 10:27:22 +0200

    test: fix test

--- src/test/kotlin/StateTest.kt
+++ src/test/kotlin/StateTest.kt
@@ -1,20 +1,24 @@
 import io.kotest.matchers.shouldBe
-import io.mockk.every
-import io.mockk.mockk
-import io.mockk.mockkClass
 import resources.TestUtils
 import kotlin.test.Test
-import resources.TestUtils.actionEmpty
 import resources.TestUtils.actionNotEmpty
-import resources.TestUtils.fluentEmpty
-import resources.TestUtils.fluentNotEmpty
+import resources.TestUtils.effectNotEmpty
+import resources.TestUtils.name
 
 class StateTest {
     private val fluent = Fluent.of(
-        TestUtils.name,
+        name,
         emptyList(),
-        TestUtils.predicateNotEmpty, true
+        TestUtils.predicateNotEmpty,
+        true
     )
+
+    private val action= Action.of(
+        name,
+        mapOf(TestUtils.variableNotEmpty to TestUtils.type1),
+        setOf(fluent),
+        setOf(effectNotEmpty
+    ))
 
     private val state = State.of(setOf(fluent))
 
@@ -30,7 +34,7 @@ class StateTest {
     @Test
     fun testNotEmptyCreation() {
         state.fluents.isEmpty() shouldBe false
-        state.isApplicable(actionNotEmpty) shouldBe true
+        state.isApplicable(action) shouldBe true
         //assertEquals(stateEmpty.apply(action), state)
     }
 }

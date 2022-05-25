4a785c64d3b4082fcc850fd5e88730baf75d76b6
Giulia <giulia.brugnatti@studio.unibo.it@users.noreply.github.com>
2022-05-25 10:05:18 +0200

    refactor: remove GoalImpl class and its dependencies

--- src/main/kotlin/Goal.kt
+++ src/main/kotlin/Goal.kt
@@ -1,13 +1,8 @@
-import impl.GoalImpl
-
 /**
  * The goal is a logical expression of predicates which must be satisfied
  * in order for a plan to be considered a solution.
  */
 interface Goal {
     fun isSatisfiedBy(state: State):Boolean
-    companion object {
-        fun of(): Goal = GoalImpl()
-    }
 }
 

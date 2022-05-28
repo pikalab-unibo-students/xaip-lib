5467a95045dfb8182ef86c3491fda6dc4eb25eca
Giulia <giulia.brugnatti@studio.unibo.it@users.noreply.github.com>
2022-05-28 16:11:29 +0200

    docs: add documentation to ActionImpl

--- src/main/kotlin/impl/ActionImpl.kt
+++ src/main/kotlin/impl/ActionImpl.kt
@@ -7,6 +7,11 @@ import VariableAssignment
 import Type
 import Variable
 
+/**
+ * An action is a quartet ⟨N, M, P, D⟩; where N is the action name, P is a map of variables and their types,
+ * P is a set of fluents representing pre-conditions,
+ * E is a set of effect, representing positive/negative post-conditions a.k.a. Add List or Remove List.
+ */
 internal data class ActionImpl(
     override val name: String,
     override val parameters: Map<Variable, Type>,

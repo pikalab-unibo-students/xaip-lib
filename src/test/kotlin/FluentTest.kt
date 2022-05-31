b348832f1ad5d0e544d5974861ced2fe2dceb4fc
Giulia <giulia.brugnatti@studio.unibo.it@users.noreply.github.com>
2022-05-31 17:59:11 +0200

    test: update fluent test

--- src/test/kotlin/FluentTest.kt
+++ src/test/kotlin/FluentTest.kt
@@ -1,12 +1,16 @@
 import io.kotest.matchers.shouldBe
+import it.unibo.tuprolog.core.Substitution
 import kotlin.test.Test
 import resources.TestUtils.fluentEmpty
 import resources.TestUtils.fluentNotEmpty
+import resources.TestUtils.getRandomString
 import resources.TestUtils.name
 import resources.TestUtils.predicateEmpty
 import resources.TestUtils.predicateNotEmpty
 import resources.TestUtils.size
+import resources.TestUtils.substitution
 import resources.TestUtils.variableNotEmpty
+import kotlin.test.assertFailsWith
 
 class FluentTest {
 
@@ -22,8 +26,39 @@ class FluentTest {
         fluentNotEmpty.name shouldBe name
         fluentNotEmpty.args.isEmpty() shouldBe false
         fluentNotEmpty.args.size shouldBe size
-        fluentNotEmpty.args.forEach{it shouldBe variableNotEmpty}
+        fluentNotEmpty.args.forEach { it shouldBe variableNotEmpty }
         (fluentNotEmpty.instanceOf == predicateNotEmpty) shouldBe true
         fluentNotEmpty.isNegated shouldBe true
+    }
+    @Test
+    fun testCommonBehaviour(){
+        fluentNotEmpty.match(fluentEmpty) shouldBe false
+        fluentNotEmpty.match(fluentNotEmpty) shouldBe true
+        fluentNotEmpty.isGround shouldBe false
+        fluentNotEmpty.not().isNegated shouldBe false
+        fluentNotEmpty.apply(substitution) shouldBe fluentNotEmpty //se non sono uguali resta quella originale why
+    }
+
+    @Test
+    fun testMgu(){
+        val variable=Variable.of(getRandomString(size))
+        val fluent:Fluent =
+            Fluent.of(name,
+                List<Value>(size){ variable},
+                predicateNotEmpty, false)
+
+        fluent.mostGeneralUnifier(fluentNotEmpty) shouldBe
+                VariableAssignment.of(variable, variableNotEmpty)
+
+        fluentNotEmpty.mostGeneralUnifier(fluentNotEmpty) shouldBe Substitution.empty()
+    }
+
+    @Test
+    fun testNotUnifiableFluent() {
+        assertFailsWith(
+            exceptionClass=NotUnifiableException::class,
+            message="No exception found",
+            block={fluentNotEmpty.mostGeneralUnifier(fluentEmpty) shouldBe Substitution.failed()}
+        )
     }
 }

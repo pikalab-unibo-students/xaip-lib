ecdb9c267b6ff96f543185a34199a2a851649a1c
Giulia <giulia.brugnatti@studio.unibo.it@users.noreply.github.com>
2022-05-31 23:19:37 +0200

    test: update effect test (apply())

--- src/test/kotlin/EffectTest.kt
+++ src/test/kotlin/EffectTest.kt
@@ -1,20 +1,37 @@
 import io.kotest.matchers.shouldBe
+import resources.TestUtils
 import kotlin.test.Test
 import resources.TestUtils.effectEmpty
 import resources.TestUtils.effectNotEmpty
 import resources.TestUtils.fluentEmpty
 import resources.TestUtils.fluentNotEmpty
+import resources.TestUtils.substitution
 
 class EffectTest {
+    private val variable=Variable.of("different value")
+
+    private val substitution2= VariableAssignment.of(TestUtils.variableNotEmpty, variable)
+    private val fluent= Fluent.of(
+        TestUtils.name, List<Value>(TestUtils.size){variable}, TestUtils.predicateNotEmpty, true)
+
+    private val effect = Effect.of(fluent, true)
 
     @Test
     fun testEmptyCreation() {
         effectEmpty.fluent shouldBe fluentEmpty
         effectEmpty.isPositive shouldBe false
+
     }
+
     @Test
     fun testNotEmptyCreation() {
         effectNotEmpty.fluent shouldBe fluentNotEmpty
         effectNotEmpty.isPositive shouldBe true
+    }
+
+    @Test
+        fun testApplyWorksAsExpected(){
+        effectNotEmpty.apply(substitution) shouldBe effectNotEmpty
+        effectNotEmpty.apply(substitution2) shouldBe effect
     }
 }

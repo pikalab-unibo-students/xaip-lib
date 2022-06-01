a83147914367136827331bd50679db03a38c43cb
Giulia <giulia.brugnatti@studio.unibo.it@users.noreply.github.com>
2022-06-01 13:27:16 +0200

    test: fix state test

--- src/test/kotlin/StateTest.kt
+++ src/test/kotlin/StateTest.kt
@@ -5,49 +5,54 @@ import kotlin.test.Test
 import resources.TestUtils.actionNotEmpty
 import resources.TestUtils.effectNotEmpty
 import resources.TestUtils.name
+import resources.TestUtils.objNotEmpty
 import resources.TestUtils.state
 import resources.TestUtils.substitution
 
 class StateTest {
     private val fluent = Fluent.of(
         name,
-        emptyList(),
+        listOf(objNotEmpty),
         TestUtils.predicateNotEmpty,
         true
     )
+    val state1= State.of(setOf(fluent))
 
     private val action= Action.of(
         name,
         mapOf(TestUtils.variableNotEmpty to TestUtils.type1),
         setOf(fluent),
-        setOf(effectNotEmpty
-    ))
-
-    //private val state = State.of(setOf(fluent))
+        setOf(effectNotEmpty)
+    )
 
     private val stateEmpty: State = State.of(emptySet())
 
     @Test
     fun testEmptyCreation() {
         stateEmpty.fluents.isEmpty() shouldBe true
-        stateEmpty.isApplicable(actionNotEmpty) shouldBe false
     }
 
     @Test
     fun testNotEmptyCreation() {
         state.fluents.isEmpty() shouldBe false
-        state.isApplicable(action) shouldBe true
+        state.fluents.forEach{it.isGround shouldBe true}
     }
 
     @Test
     fun testApplyWorksAsExpected(){
         state.apply(substitution) shouldBe state
-        //finisci il caso di sostituzione sensata
+        /*
+        finisci il caso di sostituzione sensata:
+        non capisco come conciliare che lo stato debba avere tutti i fluent ground
+         ossia che non debbano contenere nessuna variabile con il fatto che per
+         essere applicabile devo fare match tra le precondiizoni e i fluent dello stato
+         */
     }
 
     @Test
     fun testIsApplicableWorkAsExpected(){
         state.isApplicable(actionNotEmpty) shouldBe false
         state.isApplicable(actionEmpty) shouldBe true
+        state1.isApplicable(action) shouldBe true
     }
 }

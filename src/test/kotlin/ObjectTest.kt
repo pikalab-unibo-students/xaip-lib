4135477e3cfc7713f5c88a813c241a0b1cb85fe0
Giulia <giulia.brugnatti@studio.unibo.it@users.noreply.github.com>
2022-06-08 10:26:04 +0200

    test(ObjectTest): update test using TestUtilObject

--- src/test/kotlin/ObjectTest.kt
+++ src/test/kotlin/ObjectTest.kt
@@ -1,9 +1,12 @@
 import io.kotest.core.spec.style.AnnotationSpec
+import io.kotest.matchers.collections.shouldBeIn
 import io.kotest.matchers.shouldBe
 import resources.TestUtils.name
 import resources.TestUtils.objEmpty
 import resources.TestUtils.objNotEmpty
 import resources.TestUtils.substitution
+import resources.TestUtils.Values
+import resources.TestUtils.objects
 
 class ObjectTest : AnnotationSpec() {
     @Test
@@ -23,5 +26,15 @@ class ObjectTest : AnnotationSpec() {
 
         objNotEmpty.apply(substitution) shouldBe objNotEmpty
         objNotEmpty.isGround shouldBe true
+    }
+
+    @Test
+    fun testObjectTestUtilObjectWorksAsExpected() {
+        Values.arm shouldBeIn objects
+
+        Values.arm.apply(VariableAssignment.of(Values.X, Values.X)) shouldBe Values.arm
+        Values.arm.apply(VariableAssignment.of(Values.X, Values.floor)) shouldBe Values.arm
+
+        Values.arm.isGround shouldBe true
     }
 }
--- src/test/kotlin/resources/TestUtils.kt
+++ src/test/kotlin/resources/TestUtils.kt
@@ -45,7 +45,7 @@ object TestUtils {
     val variables= arrayOf(Values.W, Values.X, Values.Y, Values.Z)
     val types = arrayOf(Types.blocks, Types.locations, Types.numbers, Types.strings, Types.anything )
     val predicates= arrayOf(Predicates.at, Predicates.on, Predicates.clear, Predicates.armEmpty)
-
+    val objects = arrayOf(Object.of("a"), Object.of("b"), Object.of("c") , Object.of("floor"), Object.of("arm"), Object.of(0), Object.of(1) , Object.of(2))
     object Types {
         val anything = Type.of("anything")
         val strings = Type.of("strings", anything)

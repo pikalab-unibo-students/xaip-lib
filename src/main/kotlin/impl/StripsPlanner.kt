3fbae523d771a166ca31085b33761b8f241cf986
Giulia <giulia.brugnatti@studio.unibo.it@users.noreply.github.com>
2022-07-07 09:07:26 +0200

    fix: plan if case, change implementation using 'when' conditional expression

--- src/main/kotlin/impl/StripsPlanner.kt
+++ src/main/kotlin/impl/StripsPlanner.kt
@@ -1,6 +1,7 @@
 package impl
 
 import Action
+import Applicable
 import Effect
 import Fluent
 import FluentBasedGoal
@@ -20,61 +21,72 @@ internal class StripsPlanner : Planner {
         )
     }
 
+    private fun Stack<Applicable<*>>.apply(substitution: VariableAssignment){
+        val i = listIterator()
+        while(i.hasNext()){
+            val x= i.next()
+            i.set(x.apply(substitution))
+        }
+    }
+
     private fun plan(
         initialState: State,
         actions: Set<Action>,
         goal: FluentBasedGoal
     ): Sequence<Plan> = sequence {
         var currentState = initialState
-        val stack = Stack<Any>().also { it.push(goal) }
+        val stack = Stack<Applicable<*>>().also { it.push(goal) }
         val plan = mutableListOf<Action>()
         while (stack.isNotEmpty()) {
             val head = stack.peek()
-            if (head is Fluent){
-                stack.pop()//Aggiunto IO
-                if(currentState.fluents.any { it.match(head) }) {
-                    val substitution=currentState.fluents.filter{it.match(head)}.map{it.mostGeneralUnifier(head)}
-                for (elem in stack) {
-                    if (elem is Fluent)
-                        elem.apply(substitution[0])
-                    else if (elem is Action)
-                        elem.apply(substitution[0])
-                }
-
-                //TODO("applica la sostituzione a tutto lo stack")
-            } } else if (head is Effect && actions.any { a -> a.positiveEffects.any { it.match(head) } }) {
-                stack.pop()
-                actions.forEach { a ->
-                    a.positiveEffects.forEach {
-                        if (it.match(head)) {
-                            stack.push(a)
-                            a.preconditions.forEach { p -> stack.push(p) }
-                            if (it.match(head)) {
-                                stack.push(it)
+            when {
+                head is Fluent -> {
+                    if (currentState.fluents.any { it.match(head) }) {//secondo giro entra, terzo giro a (c, arm) non fa match nulla passa ad effect
+                        val substitution =
+                            currentState.fluents.filter { it.match(head) }.map { it.mostGeneralUnifier(head) }
+                        stack.pop()//rimuovo la testa prima di applicare la sostituzione allo stack
+                        stack.apply(substitution.first())
+                        //TODO("applica la sostituzione a tutto lo stack")
+                    } else {
+                        val h = Effect.of(head)
+                        //actions.any { a -> a.positiveEffects.any { it.match(h) } } {
+                        stack.pop()// la pop la devo fare prima se no rimuovo quello che carico
+                        actions.forEach { a ->
+                            a.positiveEffects.forEach {
+                                if (it.match(h)) {
+                                    stack.push(a)
+                                    a.preconditions.forEach { p -> stack.push(p) }
+                                    if (it.match(h)) {
+                                        stack.push(it)
+                                    }
+                                }
+                                for (elem in stack)
+                                    if (elem is FluentBasedGoal)
+                                        elem.apply(it.mostGeneralUnifier(h))
                             }
-                        }
-                        for (elem in stack)
-                            if (elem is FluentBasedGoal)
-                                elem.apply(it.mostGeneralUnifier(head))
-                    }
-                    /*TODO("retrieve dell'azione")
+                            /*TODO("retrieve dell'azione")
                     TODO("push dell'azione")
                     TODO("push delle precondizioni dell'azione")
-
                      */
+                        }
+
+                    }
                 }
-            } else if (head is FluentBasedGoal) {//non entra sebbene la testa dovrebbe essere un fluentBasedGoalImpl
-                stack.pop()
-                for (fluent in head.targets) {
-                    stack.push(fluent)
+                (head is FluentBasedGoal) -> {//prima iterazione entra qui goal-> fluent sullo stack
+                    stack.pop()//cava il fluentBasedGoal prima di mettere i Fluents
+                    for (fluent in head.targets) {
+                        stack.push(fluent)//carico i tre fluent sullo stack
+                    }
                 }
-            } else if (head is Action) {
-                stack.pop()//aggiunto IO
-                currentState = currentState.apply(head).toList()[0]
-                plan.add(head)
-                //TODO("applicare l'azione a currentState e aggiornarlo")
-            } else {
-                // do nothing
+                (head is Action) -> {
+                    stack.pop()
+                    currentState = currentState.apply(head).toList()[0]
+                    plan.add(head)
+                    //TODO("applicare l'azione a currentState e aggiornarlo")
+                }
+                else -> {
+                    // do nothing
+                }
             }
         }
         yield(Plan.of(plan))

7d3b9af994069497d110d2bcaf783868af14c792
Giulia <giulia.brugnatti@studio.unibo.it>
2022-08-04 18:26:22 +0200

    fix: detekt issue

--- src/main/kotlin/dsl/EffectsDSL.kt
+++ src/main/kotlin/dsl/EffectsDSL.kt
@@ -8,13 +8,16 @@ class EffectsDSL(
 ) {
     var effects: MutableSet<Effect> = mutableSetOf()
 
-    var predicate: String =""
+    var predicate: String = ""
     var arity: Int = 0
-    var fluent: String =""
+    var fluent: String = ""
     var isPositive = true
 
+    /**
+     * Method that allow to treat a [String] as it was a [Effect].
+     */
     operator fun String.invoke() {
-        if(predicateProvider.findPredicate(predicate, arity)==null) error ("Predicate does not exist")
+        if (predicateProvider.findPredicate(predicate, arity) == null) error("Predicate does not exist")
         effects += Effect.of(TODO())
     }
 }

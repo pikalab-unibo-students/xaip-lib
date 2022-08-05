351c6ee6d58657a4387c79b61e225c6880c7f009
Giulia <giulia.brugnatti@studio.unibo.it@users.noreply.github.com>
2022-08-05 14:17:30 +0200

    refactore: remove useless class

--- src/main/kotlin/dsl/EffectsDSL.kt
+++ src/main/kotlin/dsl/EffectsDSL.kt
@@ -8,14 +8,14 @@ import dsl.provider.PredicateProvider
  * */
 class EffectsDSL(
     predicateProvider: PredicateProvider
-): AbstractFluentDSL(predicateProvider) {
+) : AbstractFluentDSL(predicateProvider) {
     var effects: MutableSet<Effect> = mutableSetOf()
 
     operator fun Fluent.unaryPlus() = effects.add(Effect.positive(this))
-    
+
     operator fun String.unaryPlus() = effects.add(Effect.positive(this()))
 
     operator fun Fluent.unaryMinus() = effects.add(Effect.negative(this))
-    
+
     operator fun String.unaryMinus() = effects.add(Effect.negative(this()))
 }

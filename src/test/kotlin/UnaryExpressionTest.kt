781b81857990d71c7568ff7989f85fd419433f3c
Giulia <giulia.brugnatti@studio.unibo.it@users.noreply.github.com>
2022-08-07 00:04:01 +0200

    Test(UnaryExpression): update test to use Operands object

--- src/test/kotlin/UnaryExpressionTest.kt
+++ src/test/kotlin/UnaryExpressionTest.kt
@@ -1,8 +1,8 @@
 import io.kotest.core.spec.style.AnnotationSpec
 import io.kotest.matchers.shouldBe
-import resources.TestUtils.Operands
 import resources.TestUtils.Expressions
 import resources.TestUtils.Fluents
+import resources.TestUtils.Operands
 
 class UnaryExpressionTest : AnnotationSpec() {
     private val unaryExpression = UnaryExpression.of(Fluents.atXArm, Operand.of("not"))
@@ -16,15 +16,15 @@ class UnaryExpressionTest : AnnotationSpec() {
     @Test
     fun testExpressionObjectWorkAsExpected() {
         Expressions.unaryExpressionNotArmEmpty.expression shouldBe Fluents.armEmpty
-        Expressions.unaryExpressionNotArmEmpty.operand.name shouldBe Operands.not
+        Expressions.unaryExpressionNotArmEmpty.operand shouldBe Operands.not
     }
 
     @Test
     fun testComplexExpressionObjectWorkAsExpected() {
         val complexUnaryExpression = UnaryExpression.of(unaryExpression, Operand.of("not"))
         complexUnaryExpression.expression shouldBe unaryExpression
-        complexUnaryExpression.operand.name shouldBe Operands.not
+        complexUnaryExpression.operand shouldBe Operands.not
         (complexUnaryExpression.expression as UnaryExpression).expression shouldBe Fluents.atXArm
-        (complexUnaryExpression.expression as UnaryExpression).operand.name shouldBe "not"
+        (complexUnaryExpression.expression as UnaryExpression).operand shouldBe Operands.not
     }
 }

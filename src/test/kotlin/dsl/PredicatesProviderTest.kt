ac247d9e2d22465cd947bb1ddb7cc7239ae124e0
Giulia <giulia.brugnatti@studio.unibo.it@users.noreply.github.com>
2022-08-07 16:17:47 +0200

    test: add tests for predicate and types providers

--- src/test/kotlin/dsl/PredicatesProviderTest.kt
+++ src/test/kotlin/dsl/PredicatesProviderTest.kt
@@ -1,17 +1,28 @@
 package dsl
 
 import io.kotest.core.spec.style.AnnotationSpec
+import io.kotest.matchers.collections.shouldBeIn
+import io.kotest.matchers.collections.shouldNotBeIn
+import resources.TestUtils
 
 class PredicatesProviderTest : AnnotationSpec() {
     @Ignore
     @Test
     fun testPredicateExists() {
-        TODO()
+        val domain = domain {
+            // TODO("dovrai importare un object DomainDSL quando esisterà
+        }
+
+        TestUtils.Predicates.on shouldBeIn domain.predicates
     }
 
     @Ignore
     @Test
     fun testPredicateNotExists() {
-        TODO()
+        val domain = domain {
+            // TODO("dovrai importare un object DomainDSL quando esisterà
+        }
+
+        TestUtils.Predicates.at shouldNotBeIn domain.predicates
     }
 }
--- src/test/kotlin/dsl/TypesProviderTest.kt
+++ src/test/kotlin/dsl/TypesProviderTest.kt
@@ -1,17 +1,28 @@
 package dsl
 
 import io.kotest.core.spec.style.AnnotationSpec
+import io.kotest.matchers.collections.shouldBeIn
+import io.kotest.matchers.collections.shouldNotBeIn
+import resources.TestUtils
 
 class TypesProviderTest : AnnotationSpec() {
     @Ignore
     @Test
-    fun testTypeExists() {
-        TODO()
+    fun testTypesExists() {
+        val domain = domain {
+            // TODO("dovrai importare un object DomainDSL quando esisterà
+        }
+
+        TestUtils.Types.blocks shouldBeIn domain.types
     }
 
     @Ignore
     @Test
     fun testTypesNotExists() {
-        TODO()
+        val domain = domain {
+            // TODO("dovrai importare un object DomainDSL quando esisterà
+        }
+
+        TestUtils.Types.strings shouldNotBeIn domain.types
     }
 }

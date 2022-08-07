601cd21a8c8c6450a13715adaeaab040b786dd96
Giulia <giulia.brugnatti@studio.unibo.it@users.noreply.github.com>
2022-08-07 16:24:45 +0200

    chore: remove useless parameters from ActionsDSL

--- src/main/kotlin/dsl/ActionsDSL.kt
+++ src/main/kotlin/dsl/ActionsDSL.kt
@@ -13,14 +13,11 @@ class ActionsDSL(
     private val predicateProvider: PredicateProvider
 ) {
     val actions: MutableSet<Action> = mutableSetOf()
-    val parameters: Map<Variable, Type> = mutableMapOf()
-    val effects: Set<Effect> = setOf()
 
     /**
      * Method that allow to treat a [String] as it was a [Action].
      */
     operator fun String.invoke(f: ActionDSL.() -> Unit) {
         actions += ActionDSL(predicateProvider).also(f).toAction(this)
-        TODO("Capisci come aggiungere la roba anche parameter ed effect")
     }
 }

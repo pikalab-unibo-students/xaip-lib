8ef1d2836de04bade3faee2ea31839d85b9666cd
Giulia <giulia.brugnatti@studio.unibo.it@users.noreply.github.com>
2022-08-12 19:11:07 +0200

    fix(TypeProviderTest): delete useless import

--- src/test/kotlin/dsl/TypesProviderTest.kt
+++ src/test/kotlin/dsl/TypesProviderTest.kt
@@ -1,12 +1,8 @@
 package dsl
 
-import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.AnnotationSpec
 import io.kotest.matchers.collections.shouldBeIn
 import io.kotest.matchers.collections.shouldNotBeIn
-import io.kotest.matchers.string.shouldStartWith
-import resources.TestUtils
-import resources.TestUtils.Types
 import resources.TestUtils.domainDSL
 import resources.TestUtils.types
 

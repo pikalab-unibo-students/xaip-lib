08b34d54f30facac739f60c1006ee5f7ea4bcc7c
Giulia <giulia.brugnatti@studio.unibo.it@users.noreply.github.com>
2022-08-14 13:49:36 +0200

    test: update PredicateProviderTest adding tests

--- src/test/kotlin/dsl/PredicatesProviderTest.kt
+++ src/test/kotlin/dsl/PredicatesProviderTest.kt
@@ -7,7 +7,6 @@ import io.kotest.matchers.shouldBe
 import resources.TestUtils
 import resources.TestUtils.domainDSL
 
-// TODO imho thesing single dsl classes in this phase just wastes your time. I would start by test the DSL as a whole
 class PredicatesProviderTest : AnnotationSpec() {
 
     @Test
@@ -15,19 +14,23 @@ class PredicatesProviderTest : AnnotationSpec() {
         TestUtils.Predicates.armEmpty shouldBeIn domainDSL.predicates
         TestUtils.Predicates.armEmpty.name shouldBe domainDSL.predicates.last().name
         TestUtils.Predicates.armEmpty.arguments shouldBe domainDSL.predicates.last().arguments
+        TestUtils.Predicates.armEmpty.arguments.first().superType shouldBe
+            domainDSL.predicates.last().arguments.first().superType
     }
 
     @Test
     fun testOnPredicateExists() {
+        TestUtils.Predicates.on shouldBeIn domainDSL.predicates
         TestUtils.Predicates.on.name shouldBe domainDSL.predicates.first().name
+        TestUtils.Predicates.on.arguments shouldBe domainDSL.predicates.first().arguments
         TestUtils.Predicates.on.arguments[1].name shouldBe domainDSL.predicates.first().arguments[1].name
         TestUtils.Predicates.on.arguments[1].superType shouldBe domainDSL.predicates.first().arguments[1].superType
-        // TestUtils.Predicates.on shouldBeIn domainDSL.predicates
+        TestUtils.Predicates.on.arguments[2].name shouldBe domainDSL.predicates.first().arguments[2].name
+        TestUtils.Predicates.on.arguments[2].superType shouldBe domainDSL.predicates.first().arguments[2].superType
     }
 
-    @Ignore
     @Test
     fun testPredicateNotExists() {
-        TestUtils.Predicates.at shouldNotBeIn domainDSL.predicates
+        Predicate.of("nothing") shouldNotBeIn domainDSL.predicates
     }
 }

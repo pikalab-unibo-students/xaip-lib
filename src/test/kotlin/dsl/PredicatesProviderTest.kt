29a15d7bfc1bc16e8a6103bbd964eebe095bcd27
Giulia <giulia.brugnatti@studio.unibo.it@users.noreply.github.com>
2022-08-14 13:58:06 +0200

    test: fix indentation + add import in PredicateProviderTest

--- src/test/kotlin/dsl/PredicatesProviderTest.kt
+++ src/test/kotlin/dsl/PredicatesProviderTest.kt
@@ -5,29 +5,30 @@ import io.kotest.matchers.collections.shouldBeIn
 import io.kotest.matchers.collections.shouldNotBeIn
 import io.kotest.matchers.shouldBe
 import resources.TestUtils
+import resources.TestUtils.Predicates
 import resources.TestUtils.domainDSL
 
 class PredicatesProviderTest : AnnotationSpec() {
 
     @Test
     fun testArmEmptyPredicateExists() {
-        TestUtils.Predicates.armEmpty shouldBeIn domainDSL.predicates
-        TestUtils.Predicates.armEmpty.name shouldBe domainDSL.predicates.last().name
-        TestUtils.Predicates.armEmpty.arguments shouldBe domainDSL.predicates.last().arguments
+        Predicates.armEmpty shouldBeIn domainDSL.predicates
+        Predicates.armEmpty.name shouldBe domainDSL.predicates.last().name
+        Predicates.armEmpty.arguments shouldBe domainDSL.predicates.last().arguments
     }
 
     @Test
     fun testOnPredicateExists() {
-        TestUtils.Predicates.on shouldBeIn domainDSL.predicates
-        TestUtils.Predicates.on.name shouldBe domainDSL.predicates.first().name
-        TestUtils.Predicates.on.arguments shouldBe domainDSL.predicates.first().arguments
-        TestUtils.Predicates.on.arguments.first().name shouldBe domainDSL.predicates.first().arguments.first().name
-        TestUtils.Predicates.on.arguments.first().superType shouldBe
-                domainDSL.predicates.first().arguments.first().superType
-        TestUtils.Predicates.on.arguments.last().name shouldBe
-                domainDSL.predicates.first().arguments.last().name
-        TestUtils.Predicates.on.arguments.last().superType shouldBe
-                domainDSL.predicates.first().arguments.last().superType
+        Predicates.on shouldBeIn domainDSL.predicates
+        Predicates.on.name shouldBe domainDSL.predicates.first().name
+        Predicates.on.arguments shouldBe domainDSL.predicates.first().arguments
+        Predicates.on.arguments.first().name shouldBe domainDSL.predicates.first().arguments.first().name
+        Predicates.on.arguments.first().superType shouldBe
+            domainDSL.predicates.first().arguments.first().superType
+        Predicates.on.arguments.last().name shouldBe
+            domainDSL.predicates.first().arguments.last().name
+        Predicates.on.arguments.last().superType shouldBe
+            domainDSL.predicates.first().arguments.last().superType
     }
 
     @Test

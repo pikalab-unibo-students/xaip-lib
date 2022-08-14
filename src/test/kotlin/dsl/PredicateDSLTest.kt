024df1ce629f7b1858c636ed0544de93411df78e
Giulia <giulia.brugnatti@studio.unibo.it@users.noreply.github.com>
2022-08-14 12:58:13 +0200

    Revert "test: update tests"
    This reverts commit d40a1115ede3c6a0e581962ea4bdd7dc4889afb7.

--- src/test/kotlin/dsl/PredicateDSLTest.kt
+++ src/test/kotlin/dsl/PredicateDSLTest.kt
@@ -1,16 +1,17 @@
 package dsl
 
 import io.kotest.core.spec.style.AnnotationSpec
-import io.kotest.matchers.shouldBe
+import io.kotest.matchers.collections.shouldBeIn
+import io.kotest.matchers.shouldNotBe
 import resources.TestUtils.Predicates
-
-
 import resources.TestUtils.domainDSL
 
+// TODO imho thesing single dsl classes in this phase just wastes your time. I would start by test the DSL as a whole
 class PredicateDSLTest : AnnotationSpec() {
+    @Ignore
     @Test
     fun predicatesDSLworksAsExpected() {
-        domainDSL.predicates.size shouldBe 4
-        Predicates.armEmpty shouldBe domainDSL.predicates.first()
+        domainDSL.predicates.size shouldNotBe 0
+        Predicates.at shouldBeIn domainDSL.predicates
     }
 }
--- src/test/kotlin/resources/TestUtils.kt
+++ src/test/kotlin/resources/TestUtils.kt
@@ -50,16 +50,16 @@ object TestUtils {
             +"locations"("strings")
         }
         predicates {
-            +"arm_empty"()
             +"on"("blocks", "blocks")
             +"at"("blocks", "locations")
             +"clear"("blocks")
+            +"arm_empty"()
         }
         actions {
             "stack" {
                 parameters {
                     "X" ofType "blocks"
-                    "Y" ofType "locations"
+                    "Y" to "locations"
                 }
                 preconditions {
                     +"at"("X", "arm")

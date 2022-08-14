c6f38dcef28414a5c59735ebc484440113c0d861
Giulia <giulia.brugnatti@studio.unibo.it@users.noreply.github.com>
2022-08-14 17:28:06 +0200

    test: update ActionTest

--- src/test/kotlin/dsl/ActionDSLTest.kt
+++ src/test/kotlin/dsl/ActionDSLTest.kt
@@ -1,16 +1,17 @@
 package dsl
 
 import io.kotest.core.spec.style.AnnotationSpec
-import io.kotest.matchers.collections.shouldBeIn
+import io.kotest.matchers.shouldBe
 import resources.TestUtils.Actions
 import resources.TestUtils.domainDSL
 
-// TODO imho thesing single dsl classes in this phase just wastes your time. I would start by test the DSL as a whole
 class ActionDSLTest : AnnotationSpec() {
-    @Ignore
     @Test
-    fun actionDSLworksAsExpected() {
-        val actions = domainDSL.actions
-        Actions.pick shouldBeIn actions
+    fun actionsDSLworksAsExpected() {
+        domainDSL.actions.size shouldBe 1
+        domainDSL.actions.first().name shouldBe Actions.stack.name
+        domainDSL.actions.first().parameters.size shouldBe 2
+        domainDSL.actions.first().preconditions.size shouldBe 2
+        domainDSL.actions.first().effects.size shouldBe 4
     }
 }

223268f69878a86aa3c94471d28fd9c51f9f47d2
Giovanni Ciatto <giovanni.ciatto@unibo.it>
2022-09-12 15:22:38 +0200

    fix: implementation of OperatorImpl

--- src/main/kotlin/impl/ActionImpl.kt
+++ src/main/kotlin/impl/ActionImpl.kt
@@ -11,25 +11,8 @@ internal data class ActionImpl(
     override val parameters: Map<Variable, Type>,
     override val preconditions: Set<Fluent>,
     override val effects: Set<Effect>
-    // private val args: List<Value> = parameters.keys.toList()
 ) : Action {
-/*
-    override fun apply(substitution: VariableAssignment): Action =
-        copy(
-            preconditions = preconditions.map { it.apply(substitution) }.toSet(),
-            effects = effects.map { it.apply(substitution) }.toSet(),
-            args = args.map { it.apply(substitution) }
-        )
 
-    override fun refresh(scope: Scope): Action {
-        return copy(
-            // parameters = parameters.mapKeys { (k,_)-> k.refresh(scope) },
-            preconditions = preconditions.map { it.refresh(scope) }.toSet(),
-            effects = effects.map { it.refresh(scope) }.toSet(),
-            args = args.map { it.refresh(scope) }
-        )
-    }
-*/
     override val positiveEffects: Set<Effect> by lazy { effects.filter { it.isPositive }.toSet() }
 
     override val negativeEffects: Set<Effect> by lazy { effects.filterNot { it.isPositive }.toSet() }
@@ -37,35 +20,9 @@ internal data class ActionImpl(
     private fun Iterable<Fluent>.pretty(functor: String) =
         map { it.toString() }.sorted().joinToString(", ", "$functor(", ")")
 
-    @Suppress("unused")
-    val descriptor: String
-        get() = "action($name, " +
+    override fun toString(): String = "action($name, " +
             "${preconditions.pretty("if")}, " +
             "${positiveEffects.map { it.fluent }.pretty("addList")}, " +
             "${negativeEffects.map { it.fluent }.pretty("removeList")})"
 
-    // override fun toString(): String = "$name(${args.joinToString(", ") {it.toString()}})"
-    /*
-    override fun equals(other: Any?): Boolean {
-        if (this === other) return true
-        if (javaClass != other?.javaClass) return false
-
-        other as ActionImpl
-
-        if (name != other.name) return false
-        if (parameters != other.parameters) return false
-        if (preconditions != other.preconditions) return false
-        if (effects != other.effects) return false
-
-        return true
-    }
-
-    override fun hashCode(): Int {
-        var result = name.hashCode()
-        result = 31 * result + parameters.hashCode()
-        result = 31 * result + preconditions.hashCode()
-        result = 31 * result + effects.hashCode()
-        return result
-    }
-    */
 }
--- src/main/kotlin/impl/OperatorImpl.kt
+++ src/main/kotlin/impl/OperatorImpl.kt
@@ -14,10 +14,13 @@ internal data class OperatorImpl(
     override val parameters: Map<Variable, Type>,
     override val preconditions: Set<Fluent>,
     override val effects: Set<Effect>,
-    override val positiveEffects: Set<Effect>,
-    override val negativeEffects: Set<Effect>,
-    override val args: List<Value>
+    override val args: List<Value> = parameters.keys.toList()
 ) : Operator {
+
+    override val positiveEffects: Set<Effect> by lazy { effects.filter { it.isPositive }.toSet() }
+
+    override val negativeEffects: Set<Effect> by lazy { effects.filterNot { it.isPositive }.toSet() }
+
     /**
      * Method responsible for the application of a logic substitution to the entity.
      */
@@ -30,7 +33,7 @@ internal data class OperatorImpl(
 
     /**
      * Method responsible for the refreshing of variables.
-     * Mainly used to avoid spurrious substitutions.
+     * Mainly used to avoid spurious substitutions.
      */
     override fun refresh(scope: Scope): Operator {
         return copy(
@@ -42,26 +45,4 @@ internal data class OperatorImpl(
     }
 
     override fun toString(): String = "$name(${args.joinToString(", ") {it.toString()}})"
-
-    override fun equals(other: Any?): Boolean {
-        if (this === other) return true
-        if (javaClass != other?.javaClass) return false
-
-        other as OperatorImpl
-
-        if (name != other.name) return false
-        if (parameters != other.parameters) return false
-        if (preconditions != other.preconditions) return false
-        if (effects != other.effects) return false
-
-        return true
-    }
-
-    override fun hashCode(): Int {
-        var result = name.hashCode()
-        result = 31 * result + parameters.hashCode()
-        result = 31 * result + preconditions.hashCode()
-        result = 31 * result + effects.hashCode()
-        return result
-    }
 }

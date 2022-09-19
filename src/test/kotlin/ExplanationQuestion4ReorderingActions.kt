c270a546676510c6b8b9ab9cbe06f5f715c702cf
Giulia <giulia.brugnatti@studio.unibo.it@users.noreply.github.com>
2022-09-20 00:19:01 +0200

    test: update

--- src/test/kotlin/ExplanationQuestion4ReorderingActions.kt
+++ src/test/kotlin/ExplanationQuestion4ReorderingActions.kt
@@ -1,7 +1,8 @@
 import io.kotest.core.spec.style.AnnotationSpec
 import io.kotest.matchers.shouldBe
-import resources.ExplanationUtils
+import resources.ExplanationUtils.ContrastiveExplanation
 import resources.ExplanationUtils.createNewFluent
+import resources.ExplanationUtils.createNewGroundFluent
 import resources.domain.BlockWorldDomain
 import resources.domain.BlockWorldDomain.Operators.pickA
 import resources.domain.BlockWorldDomain.Operators.pickB
@@ -14,8 +15,8 @@ import resources.domain.BlockWorldDomain.Operators.stackDC
 class ExplanationQuestion4ReorderingActions : AnnotationSpec() {
     // 4.“Why is action A used before/after action B (rather than after/before)?” // reordering actions
     data class Question4(
-        val action1: List<Operator>,
-        val action2: List<Operator>,
+        val actionsToAnticipate: List<Operator>,
+        val actionsToPosticipate: List<Operator>,
         val problem: Problem,
         val originalPlan: Plan
     )
@@ -24,7 +25,7 @@ class ExplanationQuestion4ReorderingActions : AnnotationSpec() {
         listOf(pickD, stackDC),
         listOf(pickA, stackAB),
         BlockWorldDomain.Problems.stackBAstackDC,
-        Plan.of(listOf(pickA, stackAB, pickC, stackDC))
+        Plan.of(listOf(pickA, stackAB, pickD, stackDC))
     )
 
     fun newPredicate1(action: Action, string: String): Predicate =
@@ -48,7 +49,7 @@ class ExplanationQuestion4ReorderingActions : AnnotationSpec() {
         val predicates = mutableListOf<Predicate>()
         val fluents = mutableListOf<Fluent>()
 
-        //creare il DAG
+        // creare il DAG
 
         for (action in question.originalPlan.actions) {
             val predicate1 = newPredicate1(action, "ordered_")
@@ -56,38 +57,49 @@ class ExplanationQuestion4ReorderingActions : AnnotationSpec() {
             predicates.add(predicate1)
             predicates.add(predicate2)
         }
-        val indiceInizio = question.originalPlan.actions.indexOf(question.action2.first())
-        val indiceFine = question.originalPlan.actions.indexOf(question.action2.last())
-        val indiceInizio2 = question.originalPlan.actions.indexOf(question.action2.first())
-        val indiceFine2 = question.originalPlan.actions.indexOf(question.action2.last())
+        val indiceInizio = question.originalPlan.actions.indexOf(question.actionsToPosticipate.first())
+        val indiceFine = question.originalPlan.actions.indexOf(question.actionsToPosticipate.last())
+        val indiceInizio2 = question.originalPlan.actions.indexOf(question.actionsToAnticipate.first())
+        val indiceFine2 = question.originalPlan.actions.indexOf(question.actionsToAnticipate.last())
+        println("indici $indiceInizio $indiceFine $indiceInizio2 $indiceFine2")
         val newList = question.originalPlan.actions.subList(0, indiceInizio).toMutableList()
-            .also { it.addAll(question.action1)}
-            .also {it.addAll(question.originalPlan.actions.subList(indiceFine+1, indiceInizio2))  }
-            .also{it.addAll(question.action2)}
-            .also {it.addAll(question.originalPlan.actions.subList(indiceFine2, question.originalPlan.actions.size))}
+            .also { it.addAll(question.actionsToAnticipate) }
+            .also { it.addAll(question.originalPlan.actions.subList(indiceFine + 1, indiceInizio2)) }
+            .also { it.addAll(question.actionsToPosticipate) }
+            .also { it.addAll(question.originalPlan.actions.subList(indiceFine2, question.originalPlan.actions.size)) }
+        println("new list $newList")
 
-        for (action in newList){
-            fluents.add(createNewFluent(action as Operator, newPredicate1(action, "ordered_")))
-            fluents.add(createNewFluent(action as Operator, newPredicate1(action, "traversed_")))
+        for (action in newList) {
+            fluents.add(createNewGroundFluent(action as Operator, newPredicate1(action, "traversed_")))
         }
-        val newoperatorlist= mutableListOf<Action>()
-        for(i in 0 .. newList.size){
-            if(!newoperatorlist.map { it.name }.contains(newList[i].name))
+        val newoperatorlist = mutableListOf<Action>()
+        for (i in 0..newList.size - 1) {
+            if (!newoperatorlist.map { it.name }.contains(newList[i].name)) {
                 newoperatorlist.add(
-                    createNewAction1(newList[i], createNewFluent(newList[i] as Operator, newPredicate1(newList[i], "traversed_")))
+                    createNewAction1(
+                        newList[i],
+                        createNewFluent(
+                            newList[i] as Operator,
+                            newPredicate1(newList[i], "traversed_")
+                        )
+                    )
                 )
+            }
         }
-        for(action in question.problem.domain.actions){
-            if (!newoperatorlist.map { it.name.all { it.isLetter()}.toString()}.contains(action.name))
+        println(newoperatorlist)
+        for (action in question.problem.domain.actions) {
+            if (!newoperatorlist.map { it.name.all { it.isLetter() }.toString() }.contains(action.name)) {
                 newoperatorlist.add(action)
+            }
         }
         val hDomain = Domain.of(
             question.problem.domain.name,
-            predicates.also{it.addAll(question.problem.domain.predicates)}.toSet(),
-            newoperatorlist.also{it.addAll(question.problem.domain.actions)}.toSet(),
+            predicates.also { it.addAll(question.problem.domain.predicates) }.toSet(),
+            newoperatorlist.also { it.addAll(question.problem.domain.actions) }.toSet(),
             question.problem.domain.types
         )
-        val hProblem =Problem.of(
+        println(fluents)
+        val hProblem = Problem.of(
             hDomain,
             question.problem.objects,
             question.problem.initialState,
@@ -95,12 +107,12 @@ class ExplanationQuestion4ReorderingActions : AnnotationSpec() {
         )
         val plans = BlockWorldDomain.Planners.stripsPlanner.plan(hProblem)
         val hPlan = plans.first()
-        val explanation = ExplanationUtils.ContrastiveExplanation.of(question.originalPlan, hPlan, question.action1.first())
+        val explanation = ContrastiveExplanation.of(question.originalPlan, hPlan, question.actionsToAnticipate.first())
 
-        val contrastiveExplanation = ExplanationUtils.ContrastiveExplanation.of(
+        val contrastiveExplanation = ContrastiveExplanation.of(
             question.originalPlan,
             Plan.of(listOf(pickC, stackCA, pickB)),
-            question.action1.first()
+            question.actionsToAnticipate.first()
         )
         explanation shouldBe contrastiveExplanation
     }

fd434703db0706fbcb4fd5fdd03da23837fe5148
Giulia <giulia.brugnatti@studio.unibo.it>
2022-09-30 15:10:32 +0200

    core: add core classes for explanations

--- src/main/kotlin/explanation/Explanation.kt
+++ src/main/kotlin/explanation/Explanation.kt
@@ -2,6 +2,7 @@ package explanation
 
 import Operator
 import Plan
+import explanation.impl.ExplanationImpl
 
 interface Explanation {
     val originalPlan: Plan
@@ -10,4 +11,14 @@ interface Explanation {
     val addList: List<Operator>
     val deleteList: List<Operator>
     val existingList: List<Operator>
+
+    companion object {
+        /**
+         * Factory method for an [Explanation] creation.
+         */
+        fun of(
+            originalPlan: Plan,
+            novelPlan: Plan
+        ): Explanation = ExplanationImpl(originalPlan, novelPlan)
+    }
 }
---
+++ src/main/kotlin/explanation/impl/ExplanationImpl.kt
@@ -0,0 +1,52 @@
+package explanation.impl
+
+import Operator
+import Plan
+import explanation.Explanation
+
+data class ExplanationImpl(override val originalPlan: Plan, override val novelPlan: Plan) : Explanation {
+        override val addList: List<Operator> by lazy {
+            this.novelPlan.actions.filter {
+                !this.originalPlan.actions.contains(it)
+            }
+        }
+        override val deleteList: List<Operator> by lazy {
+            this.originalPlan.actions.filter {
+                !this.novelPlan.actions.contains(it)
+            }
+        }
+        override val existingList: List<Operator> by lazy {
+            this.originalPlan.actions.filter {
+                this.novelPlan.actions.contains(it)
+            }
+        }
+
+        override fun toString(): String =
+            """${ExplanationImpl::class.simpleName}(
+            |  ${ExplanationImpl::originalPlan.name}=${this.originalPlan},
+            |  ${ExplanationImpl::novelPlan.name}=${this.novelPlan},
+            |  - Diff(original plan VS new plan):
+            |  ${ExplanationImpl::addList.name}=$addList,
+            |  ${ExplanationImpl::deleteList.name}=$deleteList,
+            |  ${ExplanationImpl::existingList.name}=$existingList
+            |)
+            """.trimMargin()
+
+        override fun equals(other: Any?): Boolean {
+            if (this === other) return true
+            if (javaClass != other?.javaClass) return false
+
+            other as ExplanationImpl
+
+            if (this.originalPlan != other.originalPlan) return false
+            if (this.novelPlan != other.novelPlan) return false
+
+            return true
+        }
+
+        override fun hashCode(): Int {
+            var result = this.originalPlan.hashCode()
+            result = 31 * result + this.novelPlan.hashCode()
+            return result
+        }
+}

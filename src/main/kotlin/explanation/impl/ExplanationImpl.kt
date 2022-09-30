4c9c69937c8dba4dd07452949a2587e88187b859
Giulia <giulia.brugnatti@studio.unibo.it>
2022-09-30 15:25:10 +0200

    fix: detekt minor issues

--- src/main/kotlin/explanation/impl/ExplanationImpl.kt
+++ src/main/kotlin/explanation/impl/ExplanationImpl.kt
@@ -5,24 +5,24 @@ import Plan
 import explanation.Explanation
 
 data class ExplanationImpl(override val originalPlan: Plan, override val novelPlan: Plan) : Explanation {
-        override val addList: List<Operator> by lazy {
-            this.novelPlan.actions.filter {
-                !this.originalPlan.actions.contains(it)
-            }
+    override val addList: List<Operator> by lazy {
+        this.novelPlan.actions.filter {
+            !this.originalPlan.actions.contains(it)
         }
-        override val deleteList: List<Operator> by lazy {
-            this.originalPlan.actions.filter {
-                !this.novelPlan.actions.contains(it)
-            }
+    }
+    override val deleteList: List<Operator> by lazy {
+        this.originalPlan.actions.filter {
+            !this.novelPlan.actions.contains(it)
         }
-        override val existingList: List<Operator> by lazy {
-            this.originalPlan.actions.filter {
-                this.novelPlan.actions.contains(it)
-            }
+    }
+    override val existingList: List<Operator> by lazy {
+        this.originalPlan.actions.filter {
+            this.novelPlan.actions.contains(it)
         }
+    }
 
-        override fun toString(): String =
-            """${ExplanationImpl::class.simpleName}(
+    override fun toString(): String =
+        """${ExplanationImpl::class.simpleName}(
             |  ${ExplanationImpl::originalPlan.name}=${this.originalPlan},
             |  ${ExplanationImpl::novelPlan.name}=${this.novelPlan},
             |  - Diff(original plan VS new plan):
@@ -30,23 +30,23 @@ data class ExplanationImpl(override val originalPlan: Plan, override val novelPl
             |  ${ExplanationImpl::deleteList.name}=$deleteList,
             |  ${ExplanationImpl::existingList.name}=$existingList
             |)
-            """.trimMargin()
+        """.trimMargin()
 
-        override fun equals(other: Any?): Boolean {
-            if (this === other) return true
-            if (javaClass != other?.javaClass) return false
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
 
-            other as ExplanationImpl
+        other as ExplanationImpl
 
-            if (this.originalPlan != other.originalPlan) return false
-            if (this.novelPlan != other.novelPlan) return false
+        if (this.originalPlan != other.originalPlan) return false
+        if (this.novelPlan != other.novelPlan) return false
 
-            return true
-        }
+        return true
+    }
 
-        override fun hashCode(): Int {
-            var result = this.originalPlan.hashCode()
-            result = 31 * result + this.novelPlan.hashCode()
-            return result
-        }
+    override fun hashCode(): Int {
+        var result = this.originalPlan.hashCode()
+        result = 31 * result + this.novelPlan.hashCode()
+        return result
+    }
 }

186517e9e7def7c2133b824e45824ffa61b72757
Giulia <giulia.brugnatti@studio.unibo.it@users.noreply.github.com>
2022-10-03 12:40:05 +0200

    test: add test class for simlator

--- explanation/src/main/kotlin/explanation/impl/Simulator.kt
+++ explanation/src/main/kotlin/explanation/impl/Simulator.kt
@@ -1,6 +1,32 @@
 package explanation.impl
 
+import Operator
+import State
+
 /**
  *
  */
-class Simulator
+class Simulator {
+    class Context(val actions: List<Operator>, val state: State)
+
+    var contextList: MutableList<Context> = mutableListOf()
+
+    fun simulate(actions: List<Operator>, state: State) {
+        contextList.add(Context(actions.toMutableList(), state))
+        while (true) {
+            if (contextList.isEmpty()) break
+
+            for (actionInContext in contextList.last().actions) {
+                val s = contextList.last().state
+                contextList.removeLast()
+
+                val states = s.apply(actionInContext)
+                if (!states.toList().isEmpty()) {
+                    val actionMutable = actions.subList(1, actions.size)
+                    for (newState in states)
+                        contextList.add(Context(actionMutable, newState))
+                } else break
+            }
+        }
+    }
+}
---
+++ explanation/src/test/kotlin/explanation/SimulatorTest.kt
@@ -0,0 +1,4 @@
+package explanation
+
+class SimulatorTest {
+}

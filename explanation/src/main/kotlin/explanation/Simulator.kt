4d5b3f5121ecc6fbcbabc17c2fad987864aaae8f
Giulia <giulia.brugnatti@studio.unibo.it>
2022-10-04 14:43:04 +0200

    feat: create new simulator(GC)

--- explanation/src/main/kotlin/explanation/Simulator.kt
+++ explanation/src/main/kotlin/explanation/Simulator.kt
@@ -1,13 +1,8 @@
 package explanation
 
-import Goal
-import Operator
 import Plan
 import State
-import explanation.impl.Answer
 
 interface Simulator {
-    fun simulate(plan: Plan, state: State, goal: Goal): Boolean
-    fun simulate2(plan: Plan, state: State, goal: Goal): Answer
+    fun simulate2(plan: Plan, state: State): List<State>
 }
-
---
+++ explanation/src/main/kotlin/explanation/impl/SimulatorImpl.kt
@@ -0,0 +1,26 @@
+package explanation.impl // ktlint-disable filename
+
+import Operator
+import Plan
+import State
+import explanation.Simulator
+
+class Answer(val planIsAcceptable: Boolean, val operator: Operator? = null)
+
+class SimulatorImpl : Simulator {
+    class Context(val state: State, val depth: Int = 0)
+
+    private fun <T> MutableList<T>.pop(): T = removeAt(0)
+
+    override fun simulate2(plan: Plan, state: State): List<State> {
+        val actions = plan.actions
+        val fringe: MutableList<Context> = mutableListOf(Context(state))
+        while (fringe.none { it.depth < actions.size - 1 }) {
+            val current = fringe.pop()
+            val nextStates = current.state.apply(actions[current.depth]).map { Context(it, current.depth + 1) }
+            fringe.addAll(nextStates.toList())
+        }
+        return fringe.map { it.state }
+    }
+
+}

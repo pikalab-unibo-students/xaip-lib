91ee3247ba642515d64dccdd1b6d7d31bd0c55a0
Giulia <giulia.brugnatti@studio.unibo.it@users.noreply.github.com>
2022-10-08 18:12:47 +0200

    doc: improve documentation

--- dsl/src/main/kotlin/dsl/GoalDSL.kt
+++ dsl/src/main/kotlin/dsl/GoalDSL.kt
@@ -11,7 +11,7 @@ class GoalDSL(
     predicateProvider: PredicateProvider
 ) : dsl.AbstractFluentDSL(predicateProvider) {
     /**
-     * @property fluents: set of [Fluent] that make part of the goal.
+     * @property fluents: set of [Fluent] that compose the goal.
      */
     var fluents: MutableSet<Fluent> = mutableSetOf()
 

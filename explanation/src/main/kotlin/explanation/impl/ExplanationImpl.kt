d4ba726f4773ae1f40f5c6ee57e631db779006bb
Giulia <giulia.brugnatti@studio.unibo.it>
2022-10-13 12:03:21 +0200

    feat: add chore mothod to extend explanation

--- explanation/src/main/kotlin/explanation/impl/ExplanationImpl.kt
+++ explanation/src/main/kotlin/explanation/impl/ExplanationImpl.kt
@@ -130,13 +130,6 @@ data class ExplanationImpl(
         var operatorsMissing = minimalPlan.operators.filter { !question.plan.operators.contains(it) }
         var idempotentActionList = mutableMapOf<Operator, IdempotentOperator>()
 
-        init {
-            question.plan.operators.map { operator ->
-                if (idempotentActionList.contains(operator)) {
-                    idempotentActionList[operator]?.occurence1?.inc()
-                }
-            }
-        }
         private fun isPlanLengthAcceptable(): Boolean =
             minimalPlan.operators.size > question.plan.operators.size
 
@@ -154,10 +147,35 @@ data class ExplanationImpl(
                 operator2.preconditions.conditionMatch(operator1.preconditions) &&
                 operator1.args.all { operator2.args.contains(it) }
 
-        class IdempotentOperator() {
+        class IdempotentOperator {
             var occurence1 = 0
             var operator2: Operator? = null
             var occurence2 = 0
+        }
+
+        fun idempotentList() {
+            // inizializzola listo con gli operatori che devo avere perché il piano possa essere ritenuto valido
+            for (operator in minimalPlan.operators) {
+                idempotentActionList[operator] = IdempotentOperator()
+            }
+            // vado a controllare gli operatori nel piano che mi passa l'utonto
+            for (operatorInUserPlan in question.plan.operators) {
+                // se l'operator non c'è allora devo controllare se è idempotente rispetto a qualche operator
+                // essenziale per l'accettazione del piano
+                if (!idempotentActionList.contains(operatorInUserPlan)) {
+                    for (essentialOperator in idempotentActionList) {
+                        if (isIdempotentActions(essentialOperator.key, operatorInUserPlan)) {
+                            // potrei evitare di sovrascrivere tutte le volte ma per ora va bene così
+                            idempotentActionList[essentialOperator.key]?.operator2 = operatorInUserPlan
+                            idempotentActionList[essentialOperator.key]?.occurence2?.inc()
+                        }
+                    }
+                } // se invece contiene l'elemento aggiorno il contatore
+                else idempotentActionList[operatorInUserPlan]?.occurence1?.inc()
+            }
+            // filtro mantenendo solo gli operatori che mi invalidano il piano
+            idempotentActionList = idempotentActionList
+                .filter { it.value.occurence1 <= it.value.occurence2 } as MutableMap<Operator, IdempotentOperator>
         }
     }
 }

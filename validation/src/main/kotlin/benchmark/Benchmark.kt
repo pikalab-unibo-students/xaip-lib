9d3b3dea95f7f3ada038202bb151aba7f2a2cd37
Giulia <giulia.brugnatti@studio.unibo.it>
2022-11-17 18:01:30 +0100

    style: remove useless modules

--- validation/src/main/kotlin/benchmark/Benchmark.kt
+++
@@ -1,9 +0,0 @@
-/**
- * Interface for a general benchmark.
- */
-interface Benchmark {
-    /**
-     * Method responsible for writing the test result in a given location.
-     */
-    fun write(filename: String?)
-}

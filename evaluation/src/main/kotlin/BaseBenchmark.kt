56f16bac3d2e90a7dd48ea118b1739b9bb4ccaa5
Giulia <giulia.brugnatti@studio.unibo.it@users.noreply.github.com>
2022-11-21 23:28:28 +0100

    ci: disable ci for this branch

--- evaluation/src/main/kotlin/BaseBenchmark.kt
+++ evaluation/src/main/kotlin/BaseBenchmark.kt
@@ -3,11 +3,43 @@ import core.Plan
 import core.Problem
 import core.utility.then
 import domain.BlockWorldDomain
-import domain.LogisticDomain.Operators
+import domain.BlockWorldDomain.Operators.pickA
+import domain.BlockWorldDomain.Operators.pickB
+import domain.BlockWorldDomain.Operators.pickC
+import domain.BlockWorldDomain.Operators.pickD
+import domain.BlockWorldDomain.Operators.putdownA
+import domain.BlockWorldDomain.Operators.putdownB
+import domain.BlockWorldDomain.Operators.putdownC
+import domain.BlockWorldDomain.Operators.putdownD
+import domain.BlockWorldDomain.Operators.stackAB
+import domain.BlockWorldDomain.Operators.stackAC
+import domain.BlockWorldDomain.Operators.stackAD
+import domain.BlockWorldDomain.Operators.stackBA
+import domain.BlockWorldDomain.Operators.stackBC
+import domain.BlockWorldDomain.Operators.stackBD
+import domain.BlockWorldDomain.Operators.stackCA
+import domain.BlockWorldDomain.Operators.stackCB
+import domain.BlockWorldDomain.Operators.stackCD
+import domain.BlockWorldDomain.Operators.stackDA
+import domain.BlockWorldDomain.Operators.stackDB
+import domain.BlockWorldDomain.Operators.stackDC
+import domain.BlockWorldDomain.Operators.unstackAB
+import domain.BlockWorldDomain.Operators.unstackAC
+import domain.BlockWorldDomain.Operators.unstackAD
+import domain.BlockWorldDomain.Operators.unstackBA
+import domain.BlockWorldDomain.Operators.unstackBC
+import domain.BlockWorldDomain.Operators.unstackBD
+import domain.BlockWorldDomain.Operators.unstackCA
+import domain.BlockWorldDomain.Operators.unstackCB
+import domain.BlockWorldDomain.Operators.unstackCD
+import domain.BlockWorldDomain.Operators.unstackDA
+import domain.BlockWorldDomain.Operators.unstackDB
 import explanation.Question
 import explanation.impl.* // ktlint-disable no-wildcard-imports
 import java.io.FileOutputStream
 import java.io.OutputStream
+import domain.BlockWorldDomain.Operators as bwOperators
+import domain.LogisticDomain.Operators as lOperators
 
 open class BaseBenchmark(
     problem: Problem,
@@ -16,10 +48,30 @@ open class BaseBenchmark(
     private val explanationType: String = ""
 ) : AbstractBenchmark(problem, length) {
     lateinit var question: Question
+    val idempotentActionSetBlockWorld by lazy {
+        listOf(
+            listOf(pickA, putdownA),
+            listOf(pickB, putdownB),
+            listOf(pickC, putdownC),
+            listOf(pickD, putdownD),
+            listOf(pickA, stackAB, unstackAB),
+            listOf(pickA, stackAC, unstackAC),
+            listOf(pickA, stackAD, unstackAD),
+            listOf(pickB, stackBA, unstackBA),
+            listOf(pickB, stackBC, unstackBC),
+            listOf(pickB, stackBD, unstackBD),
+            listOf(pickC, stackCA, unstackCA),
+            listOf(pickC, stackCB, unstackCB),
+            listOf(pickC, stackCD, unstackCD),
+            listOf(pickD, stackDA, unstackDA),
+            listOf(pickD, stackDB, unstackDB),
+            listOf(pickD, stackDC, bwOperators.unstackDC)
+        )
+    }
     private val operator by lazy {
         when (problem.domain.name) {
             "block_world" -> BlockWorldDomain.Operators.pickA
-            "logistic_world" -> (flag == 1).then(Operators.moveRfromL1toL3) ?: Operators.moveRfromL1toL2
+            "logistic_world" -> (flag == 1).then(lOperators.moveRfromL1toL3) ?: lOperators.moveRfromL1toL2
             else -> throw NoSuchElementException("Domain ${problem.domain.name} is not supported")
         }
     }
@@ -89,5 +141,20 @@ open class BaseBenchmark(
                 it
             ).apply { writeCsv(resultsMemory, resultsTime, flag) }
         }
+    }
+
+    fun createPlan(problem: Problem, maxLength: Int): MutableList<MutableList<Operator>> {
+        val plans = mutableListOf(mutableListOf(pickA, putdownA))
+        for (plan in plans) {
+            if (problem.domain.name == "bockWorld_domain") {
+                for (seq in idempotentActionSetBlockWorld) {
+                    val list = mutableListOf<Operator>()
+                    for (elem in seq) { list.add(elem) }
+                    plans.add(list)
+                }
+                if (plans.last().size > maxLength) break
+            } else { } // Logistic domain
+        }
+        return plans
     }
 }
